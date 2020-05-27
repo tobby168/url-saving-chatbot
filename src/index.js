c4b4e4ea541a4d702e23a4fcc8a1473518b6fba2
吳冠言 <danielwu@wuguanyandeMacBook-Air.local>
2020-05-27 20:28:47 +0800

    add keyword parser for saving url

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const { router, text } = require('bottender/router');
 const { PythonShell } = require('python-shell')
+const mysql = require('mysql');
 
 async function SayHi(context) {
   await context.sendText('Hi!');
 }
 
+async function GetUserID(event) {
+  await event.source.userId;
+}
+
 async function ParseUrl(context) {
-  const query = context.event.text;
+  let query = context.event.text;
   context.setState({
     url: query.includes(':') ? query.split(':')[1] : query.split('：')[1],
     onSave: true,
@@ -20,9 +25,49 @@ async function ConfirmUrl(context) {
     context.setState({
       onSave: false,
     });
-    await context.sendText('請重新輸入指令');
+    await context.sendText(`請重新輸入指令`);
   }
 }
+
+async function SearchUrl(context){
+  let query = context.event.text;
+  context.setState({
+    url: query.includes(':') ? query.split(':')[1] : query.split('：')[1],
+    onRead: true,
+  }) ;
+  await SearchUrlinDB(context);
+}
+
+async function SearchUrlinDB(context){
+  let con = mysql.createConnection({
+    host: "35.194.253.30",
+    user: "root",
+    password: "chatbot",
+    database: "chatbot_db"
+    });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("SELECT url FROM chatbot_db WHERE tag = '${context.state.keyword}'", function (err, result) {
+      if (err) throw err;
+      let urlResults = JSON.parse(result);
+      let count = 0;
+      await context.sendText(`關於 ${context.state.keyword} 的結果如下：`);
+      for (url in urlResults){
+        await context.sendText(`url + "\n"`);
+        count ++;
+        }
+    });
+    })
+    context.setState({
+      keyword: '',
+      onRead: false,
+    });
+    await context.sendText(`總共找到 ${count} 個結果`);
+  };
+  
+
 
 async function SaveUrl(context) {
   await context.sendText('開始儲存網址');
@@ -45,33 +90,58 @@ async function SaveUrl(context) {
   await crawlUrlByPython
     .then(async function(crawlingResults){
       await context.sendText(crawlingResults.title);
-      //[todo] get user information
+      let keyword = crawlingResults.keywords.split(“,”);
 
+      //[todo] get user information
+      let name = GetUserID(context)
 
       //[todo] save data to database
 
-
-      context.setState({
-        url: '',
-        onSave: false,
+      let con = mysql.createConnection({
+      host: "35.194.253.30",
+      user: "root",
+      password: "chatbot",
+      database: "chatbot_db"
       });
-      await context.sendText('儲存網址完畢');
 
-    })
-    .catch(async function(err){
-      await context.sendText(err);
-    })
+      con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        for (word in keyword){
+          let sql = "INSERT INTO chatbot_db (userId, url, tag) VALUES (name, context.state.url, word)";
+          con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+          }
+        });
+      });
+      
+
+  context.setState({
+    url: '',
+    onSave: false,
+  });
+  await context.sendText('儲存網址完畢');
+
+  })
+
+  .catch(async function(err){
+    await context.sendText(err);
+  })
 }
 
 module.exports = async function App() {
+
   return router([
     text(/hi|hello/, SayHi),
     text(/(存|save|Save)(:|：).*/, ParseUrl),
     text(/(是|否)/, ConfirmUrl),
+    text(/(查|找｜查詢)(:|：),*/, SearchUrl),
   ]);
 };
 
 module.exports.initialState = {
   url: '',
+  keyword: ''
   onSave: false,
 };
--- src/main.py
+++ src/main.py
@@ -1,19 +1,55 @@
 import sys 
-import json
+import json'
+import time
+import requests
+import validators
+from urllib.request import Request, urlopen
+import urllib
+from bs4 import BeautifulSoup
+import jieba
+import jieba.analyse
 
 url = sys.argv[1]
 
 #finish the code here
 
 
+tags_dic = set()
+tags_need = ""
+
+with open('tags.txt', 'r') as tags:
+    for tag in tags.readlines():
+        tags_dic.add(tag)
+
+tags = open('tags.txt', 'a')
+
+
+requests.adapters.DEFAULT_RETRIES = 5 # 增加重连次数
+s = requests.session()
+s.keep_alive = False # 关闭多余连接
+s.get(url)
+re = requests.get(url)
+re.encoding = 'utf-8' #需要新增這一行，告知html檔案解碼方式
+soup = BeautifulSoup(re.text, 'html.parser')
+url_txt = soup.text
+tag = jieba.analyse.extract_tags(url_txt, topK=20)
+for i in tag:
+    if i not in tags_dic:
+        tags_need += i + ","
+print(url +"已加入")
+
+tags.close()
+
 
 #expected results
 result = {
   'title': 'title of ' + url,
-  'keywords': ''
+  'keywords': tags_need
 }
 
 json = json.dumps(result)
 
 print(str(json))
 sys.stdout.flush()
+
+
