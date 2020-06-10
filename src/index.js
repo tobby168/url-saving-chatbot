e29a2da36b88c53c0538315f865a60073a1eeeb9
吳冠言 <danielwu@wuguanyandeMacBook-Air.local>
2020-06-10 21:10:19 +0800

    finish the project with saving and searching function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
-const { router, text } = require('bottender/router');
-const { PythonShell } = require('python-shell')
+const {
+  router,
+  text
+} = require('bottender/router');
+const {
+  PythonShell
+} = require('python-shell')
 const mysql = require('mysql');
+const fs = require('fs');
+const sd = require('silly-datetime');
+
 
 async function SayHi(context) {
   await context.sendText('Hi!');
 }
 
-async function GetUserID(event) {
-  await event.source.userId;
-}
-
 async function ParseUrl(context) {
   let query = context.event.text;
   context.setState({
-    url: query.includes(':') ? query.split(':')[1] : query.split('：')[1],
+    url: query.substr(query.indexOf('：') + 1 || query.indexOf(':') + 1),
     onSave: true,
   });
   await context.sendText(`你想要存的網址是：${context.state.url}？ 請回答「是」或「否」`);
 }
 
 async function ConfirmUrl(context) {
-  if (context.state.onSave && context.event.text === '是') await SaveUrl(context);
-  else {
+  if (context.state.onSave && (context.event.text === '是' || context.event.text === 'y')) {
+    await SaveUrl(context);
+  } else {
     context.setState({
       onSave: false,
     });
@@ -29,119 +34,153 @@ async function ConfirmUrl(context) {
   }
 }
 
-async function SearchUrl(context){
+async function SaveUrl(context) {
+  if (context.state.url.search("https://") == -1)
+    urlToSave = "https://" + context.state.url;
+  else
+    urlToSave = context.state.url;
+  const options = {
+    args: [
+      urlToSave,
+    ],
+    mode: 'text',
+  }
+  const crawlUrlByPython = async () => {
+    console.log('running python file...');
+    PythonShell.run(__dirname + '/main.py', options, (err, data) => {
+      console.log('python finished')
+      const saveDataToDB = async () => {
+        console.log('start to save to db')
+
+        //[todo] get user information
+        name = context.session.id;
+        time = sd.format(new Date(), 'YYYY-MM-DD HH:mm:ss');
+        //[todo] save data to database
+        let con = mysql.createConnection({
+          host: process.env.HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DATABASE
+        });
+        await con.connect(function (err) {
+          if (err) throw err;
+          console.log("Connected!");
+          con.query(`SELECT url FROM urls WHERE userId = "${name}"`, async function (err, result) {
+            if (err) throw err;
+            if (result.length != 0) {
+              for (i = 0; i < result.length; i++) {
+                if (result[i].url == urlToSave) {
+                  context.sendText(`此網址已存在`);
+                  context.setState({
+                    url: '',
+                    onSave: false,
+                  });
+                  console.log("state已初始化")
+                  return
+                }
+              };
+            }
+            fs.readFile(__dirname + '/temp.txt', function (error, data) {
+              // 若錯誤 error 為一個物件，則會在這邊觸發內部程式碼，作為簡單的錯誤處理
+              if (error) {
+                console.log('讀取檔案失敗')
+                return
+              }
+              var lines = data.toString()
+              var NewArray = new Array();
+              var NewArray = lines.split("\n");
+              for (i = 0; i < NewArray.length; i++) {
+                let sql = `INSERT INTO urls (userId, url, tag, insertTime) VALUES ('${name}', '${urlToSave}', '${NewArray[i]}', '${time}')`;
+                // console.log(NewArray[i])
+                con.query(sql, function (err) {
+                  if (err) throw err;
+                });
+              }
+              console.log("record inserted");
+              fs.writeFile(__dirname + '/temp.txt', '', async function () {
+                await context.sendText('儲存網址完畢');
+                await context.setState({
+                  url: '',
+                  onSave: false,
+                });
+                await console.log("state已初始化")
+              });
+            })
+          })
+        })
+      }
+      saveDataToDB();
+      //if (err) 
+      // console.log(data);
+      // const crawlingResults = JSON.parse(data);
+    })
+  }
+  crawlUrlByPython();
+}
+
+
+
+async function SearchUrl(context) {
   let query = context.event.text;
   context.setState({
-    url: query.includes(':') ? query.split(':')[1] : query.split('：')[1],
-    onRead: true,
-  }) ;
+    keyword: query.includes(':') ? query.split(':')[1] : query.split('：')[1]
+  });
   await SearchUrlinDB(context);
 }
 
-async function SearchUrlinDB(context){
+async function SearchUrlinDB(context) {
   let con = mysql.createConnection({
-    host: "35.194.253.30",
-    user: "root",
-    password: "chatbot",
-    database: "chatbot_db"
-    });
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE
+  });
 
-  con.connect(function(err) {
+  await con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query("SELECT url FROM chatbot_db WHERE tag = '${context.state.keyword}'", function (err, result) {
+    con.query(`SELECT url FROM urls WHERE tag = "${context.state.keyword}" and userId = "${context.session.id}"`, function (err, result) {
       if (err) throw err;
-      let urlResults = JSON.parse(result);
-      let count = 0;
-      await context.sendText(`關於 ${context.state.keyword} 的結果如下：`);
-      for (url in urlResults){
-        await context.sendText(`url + "\n"`);
-        count ++;
-        }
-    });
-    })
-    context.setState({
-      keyword: '',
-      onRead: false,
-    });
-    await context.sendText(`總共找到 ${count} 個結果`);
-  };
-  
-
-
-async function SaveUrl(context) {
-  await context.sendText('開始儲存網址');
-
-  const options = {
-    args:
-      [
-        context.state.url,
-      ],
-  }
-
-  const crawlUrlByPython = new Promise(function (resolve, reject) {
-    PythonShell.run(__dirname + '/main.py', options, (err, data) => {
-      if (err) reject(err);
-      const crawlingResults = JSON.parse(data)
-      resolve(crawlingResults);
-    })
-  })
-
-  await crawlUrlByPython
-    .then(async function(crawlingResults){
-      await context.sendText(crawlingResults.title);
-      let keyword = crawlingResults.keywords.split(“,”);
-
-      //[todo] get user information
-      let name = GetUserID(context)
-
-      //[todo] save data to database
-
-      let con = mysql.createConnection({
-      host: "35.194.253.30",
-      user: "root",
-      password: "chatbot",
-      database: "chatbot_db"
-      });
-
-      con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        for (word in keyword){
-          let sql = "INSERT INTO chatbot_db (userId, url, tag) VALUES (name, context.state.url, word)";
-          con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("1 record inserted");
-          }
+      // console.log(result)
+      // let urlResults = JSON.parse(result);
+      // console.log(urlResult)
+      var count = 0;
+      if (result.length == 0) {
+        context.sendText(`沒有關於${context.state.keyword}的結果`)
+        context.setState({
+          keyword: '',
         });
-      });
-      
-
-  context.setState({
-    url: '',
-    onSave: false,
-  });
-  await context.sendText('儲存網址完畢');
-
+      } else {
+        var text = `關於 ${context.state.keyword} 的結果如下：`+"\n"
+        for (i = 0; i < result.length; i++) {
+          text += result[i].url + "\n"
+          count++;
+        }
+        text += `總共找到 ${count} 個結果`;
+        context.sendText(`${text}`);
+        setTimeout(function (count) {
+          context.setState({
+            keyword: '',
+          });
+          console.log("state已初始化")
+        }, 300);
+      }
+    });
   })
 
-  .catch(async function(err){
-    await context.sendText(err);
-  })
-}
+};
 
 module.exports = async function App() {
-
   return router([
-    text(/hi|hello/, SayHi),
     text(/(存|save|Save)(:|：).*/, ParseUrl),
-    text(/(是|否)/, ConfirmUrl),
-    text(/(查|找｜查詢)(:|：),*/, SearchUrl),
+    text(/(查|找|查詢|s)(:|：),*/, SearchUrl),
+    text(/(是|否|y|n)/, ConfirmUrl),
+    text(/hi|hello/, SayHi),
   ]);
 };
 
 module.exports.initialState = {
   url: '',
-  keyword: ''
+  keyword: '',
   onSave: false,
 };
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import sys 
-import json'
+import json
 import time
 import requests
 import validators
@@ -8,6 +8,7 @@ import urllib
 from bs4 import BeautifulSoup
 import jieba
 import jieba.analyse
+import os
 
 url = sys.argv[1]
 
@@ -15,14 +16,24 @@ url = sys.argv[1]
 
 
 tags_dic = set()
-tags_need = ""
 
-with open('tags.txt', 'r') as tags:
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+tags_path = "tags.txt"
+temp_path = "temp.txt"
+TWdict_path = "TWdict.txt"
+temp_abs_path = os.path.join(script_dir, temp_path)
+tags_abs_path = os.path.join(script_dir, tags_path)
+TWdict_abs_path = os.path.join(script_dir, TWdict_path)
+jieba.set_dictionary(TWdict_abs_path)
+
+
+with open(tags_abs_path, 'r') as tags:
     for tag in tags.readlines():
+      if tag not in tags_dic:
         tags_dic.add(tag)
 
-tags = open('tags.txt', 'a')
-
+temp = open(temp_abs_path, 'a')
+temp.truncate()
 
 requests.adapters.DEFAULT_RETRIES = 5 # 增加重连次数
 s = requests.session()
@@ -32,24 +43,26 @@ re = requests.get(url)
 re.encoding = 'utf-8' #需要新增這一行，告知html檔案解碼方式
 soup = BeautifulSoup(re.text, 'html.parser')
 url_txt = soup.text
-tag = jieba.analyse.extract_tags(url_txt, topK=20)
+tag = jieba.analyse.extract_tags(url_txt, topK=100)
+tags_added = set()
 for i in tag:
-    if i not in tags_dic:
-        tags_need += i + ","
+    if i not in tags_dic and i not in tags_added:
+        temp.write(i+'\n')
+        tags_added.add(i)
 print(url +"已加入")
 
-tags.close()
+temp.close()
 
 
 #expected results
-result = {
-  'title': 'title of ' + url,
-  'keywords': tags_need
-}
+# result = {
+#   'title': 'title of ' + url,
+#   'keywords': tags_need
+# }
 
-json = json.dumps(result)
+# json = json.dumps(result)
 
-print(str(json))
-sys.stdout.flush()
+# print(str(json))
+# sys.stdout.flush()
 
 
